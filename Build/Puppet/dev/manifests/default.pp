25aea4eaa1e4242f4254100f16e2641d3795457f
Jesper De Temmerman <jesper@toron.be>
2016-05-16 13:12:07 +0200

    Added settings and installation steps to fully configure the application upon 'vagrant up'

--- Build/Puppet/dev/manifests/default.pp
+++ Build/Puppet/dev/manifests/default.pp
@@ -49,3 +49,14 @@ class
 { 
 	'phpunit':
 }
+
+exec { 'composer install':
+  command => "/usr/local/bin/composer install",
+  cwd => "/vagrant/Source",
+  environment => ["COMPOSER_HOME=/tmp"],
+}
+
+exec { 'migrate databases':
+  command => "php bin/console doctrine:migrations:migrate -n",
+  cwd => "/vagrant/Source",
+}
