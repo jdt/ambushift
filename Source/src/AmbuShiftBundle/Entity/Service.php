7f95e579ebc99e35b1c1583168d0cf0804c9e212
Jesper De Temmerman <jesper@toron.be>
2016-05-23 19:09:46 +0200

    Put the list of shifts on the service

--- Source/src/AmbuShiftBundle/Entity/Service.php
+++ Source/src/AmbuShiftBundle/Entity/Service.php
@@ -5,9 +5,16 @@ class Service
 {
     private $id;
     private $description;
+    private $shifts;
 
     public function __construct($description)
     {
     	$this->description = $description;
+        $this->shifts = new ArrayCollection();
+    }
+
+    public function getShifts()
+    {
+        return $this->shifts->toArray();
     }
 }
