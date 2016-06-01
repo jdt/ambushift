da30c1b61da405e3cb3f2c09ba6a39b732aed021
Jesper De Temmerman <jesper@toron.be>
2016-06-01 21:29:01 +0200

    Moved the test to the right directory and removed an obsolete test

--- Source/tests/AmbuShiftBundle/Repository/OperatingMonthRepositoryTest.php
+++ Source/tests/AmbuShiftBundle/Repository/OperatingMonthRepositoryTest.php
@@ -4,8 +4,6 @@ namespace AmbuShiftBundle\Tests\Repository;
 use AmbuShiftBundle\Repository\OperatingMonthRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-use \Doctrine\DBAL\Logging\DebugStack;
-
 class OperatingMonthRepositoryTest extends KernelTestCase
 {
     private $em;
@@ -16,9 +14,6 @@ class OperatingMonthRepositoryTest extends KernelTestCase
         self::bootKernel();
         $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
         $this->repo = new OperatingMonthRepository($this->em);
-
-        $this->stack = new DebugStack();
-        $this->em->getConnection()->getConfiguration()->setSQLLogger($this->stack);
     }
 
     protected function tearDown()
