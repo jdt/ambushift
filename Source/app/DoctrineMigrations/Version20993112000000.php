e3ed589ebd2d8ca3438311479a26bd2261270cf4
Jesper De Temmerman <jesper@toron.be>
2016-06-03 19:43:23 +0200

    Added some more data

--- Source/app/DoctrineMigrations/Version20993112000000.php
+++ Source/app/DoctrineMigrations/Version20993112000000.php
@@ -38,11 +38,20 @@ class Version20993112000000 extends AbstractMigration implements ContainerAwareI
         $this->addSql('INSERT INTO ambushift_crew_position(id, description, vehicleId) VALUES (1, "Driver", 1);');
         $this->addSql('INSERT INTO ambushift_crew_position(id, description, vehicleId) VALUES (2, "Attendant", 1);');
         $this->addSql('INSERT INTO ambushift_crew_position(id, description, vehicleId) VALUES (3, "Trainee", 1);');
+        $this->addSql('INSERT INTO ambushift_crew_position(id, description, vehicleId) VALUES (4, "Backup", 1);');
 
         $this->addSql('INSERT INTO ambushift_operating_month(id, year, month, serviceId) VALUES (1, 2016, 5, 1);');
 
         $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (1, "2016-05-01 18:00:00", "2016-05-02 06:00:00", 1, 1);');
-
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (2, "2016-05-02 18:00:00", "2016-05-03 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (3, "2016-05-03 18:00:00", "2016-05-04 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (4, "2016-05-04 18:00:00", "2016-05-05 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (5, "2016-05-05 18:00:00", "2016-05-06 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (6, "2016-05-06 18:00:00", "2016-05-07 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (7, "2016-05-07 18:00:00", "2016-05-08 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (8, "2016-05-08 18:00:00", "2016-05-09 06:00:00", 1, 1);');
+        $this->addSql('INSERT INTO ambushift_shift(id, `from`, `to`, vehicleId, operatingMonthId) VALUES (9, "2016-05-09 18:00:00", "2016-05-10 06:00:00", 1, 1);');
+        
         $this->addSql('INSERT INTO ambushift_shift_worker(id, userId, crewPositionId, shiftId) VALUES (1, 2, 1, 1);');
     }
 
