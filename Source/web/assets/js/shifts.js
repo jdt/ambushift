38ba19cd362397bc25ad29a41cfd7dacdae579e0
Jesper De Temmerman <jesper@toron.be>
2016-06-08 21:29:37 +0200

    Enrolling in an available position for a shift now displays a confirmation popup

--- Source/web/assets/js/shifts.js
+++ Source/web/assets/js/shifts.js
@@ -1,10 +1,26 @@
 var $ = require("jquery");
+var sprintf = require("sprintf");
 
 var Shifts = 
 {
-	initialize: function(tableSelector)
+	Messages:
 	{
+		ConfirmEnrollment: ""
+	},
 
+	initialize: function(componentSelector)
+	{
+		table = $(componentSelector + " table");
+		popup = $(componentSelector + " div:first");
+
+		var self = this;
+		table.find("a[data-type='enrollButton']").on("click", function(event) {
+			var button = $(event.target);
+			var message = sprintf(self.Messages.ConfirmEnrollment, {date: button.data("date").toLowerCase(), crewPosition: button.data("crewposition")});
+			$(componentSelector).find("span[data-type='shiftEnrollmentMessage']").text(message);
+			
+			popup.modal("show");
+		});
 	}
 };
 
