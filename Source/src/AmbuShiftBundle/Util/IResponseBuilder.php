f032ca99756ac03f7eb38cd998581c06185785ce
Jesper De Temmerman <jesper@toron.be>
2016-06-12 17:51:14 +0200

    Added a redirect method to the responsebuilder

--- Source/src/AmbuShiftBundle/Util/IResponseBuilder.php
+++ Source/src/AmbuShiftBundle/Util/IResponseBuilder.php
@@ -4,4 +4,5 @@ namespace AmbuShiftBundle\Util;
 interface IResponseBuilder
 {
     public function asView($viewName, $args = array());
+    public function asRedirect($route, $args = array());
 }
--- Source/src/AmbuShiftBundle/Util/ResponseBuilder.php
+++ Source/src/AmbuShiftBundle/Util/ResponseBuilder.php
@@ -2,18 +2,27 @@
 namespace AmbuShiftBundle\Util;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ResponseBuilder implements IResponseBuilder
 {
     private $templating;
+    private $routerInterface;
 
-    public function __construct(EngineInterface $templating)
+    public function __construct(EngineInterface $templating, RouterInterface $routerInterface)
     {
         $this->templating = $templating;
+        $this->routerInterface = $routerInterface;
     }
 
     public function asView($viewName, $args = array())
     {
         return $this->templating->renderResponse($viewName, $args);
+    }
+
+    public function asRedirect($route, $args = array())
+    {
+        return new RedirectResponse($this->routerInterface->generate($route, $args));
     }
 }
