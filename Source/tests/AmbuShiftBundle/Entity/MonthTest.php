255f001436e02081959888fe821929f773edb28c
Jesper De Temmerman <jesper@toron.be>
2016-06-13 21:52:10 +0200

    Added some more cases

--- Source/tests/AmbuShiftBundle/Entity/MonthTest.php
+++ Source/tests/AmbuShiftBundle/Entity/MonthTest.php
@@ -10,11 +10,16 @@ class MonthTest extends PHPUnit_Framework_TestCase
     public function testIsValid()
     {
         $this->assertTrue(Month::isValid(2016, 2));
+        $this->assertTrue(Month::isValid("2016", "2"));
+        $this->assertTrue(Month::isValid(2016, "2"));
         $this->assertFalse(Month::isValid(2016, null));
         $this->assertFalse(Month::isValid(null, 2));
         $this->assertFalse(Month::isValid(null, null));
         $this->assertFalse(Month::isValid("aa", 2));
         $this->assertFalse(Month::isValid(2016, "aa"));
         $this->assertFalse(Month::isValid("aa", "aa"));
+        $this->assertFalse(Month::isValid(2016, 0));
+        $this->assertFalse(Month::isValid(2016, 13));
+        $this->assertFalse(Month::isValid(-1, 13));
     }
 }
