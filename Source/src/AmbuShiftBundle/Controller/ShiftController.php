dddea01f99103d7f8a39edd09483fb7a8e2e0e57
Jesper De Temmerman <jesper@toron.be>
2016-06-13 22:40:23 +0200

    Shiftcontroller should display the passed operating month or the current month if no valid month was passed. If the operating month does not exist, simply return an empty one

--- Source/src/AmbuShiftBundle/Controller/ShiftController.php
+++ Source/src/AmbuShiftBundle/Controller/ShiftController.php
@@ -7,6 +7,7 @@ use AmbuShiftBundle\Repository\IOperatingMonthRepository;
 use AmbuShiftBundle\Service\ICalendar;
 use AmbuShiftBundle\ViewModel\ShiftViewModel;
 use AmbuShiftBundle\Entity\Month;
+use AmbuShiftBundle\Entity\OperatingMonth;
 
 class ShiftController
 {
@@ -21,15 +22,25 @@ class ShiftController
 		$this->calendar = $calendar;
 	}
 
-    public function indexAction()
+    public function indexAction($year = null, $month = null)
     {
-    	$now = $this->calendar->getCurrentDate();
+        if(Month::isValid($year, $month))
+        {
+            $selectedMonth = new Month($year, $month);
+        }
+        else
+        {
+            $now = $this->calendar->getCurrentDate();
+            $selectedMonth = new Month($now->format("Y"), $now->format("n"));
+        }
 
-    	$month = $this->operatingMonthRepository->getOperatingMonth(new Month($now->format("Y"), $now->format("n")));
-        
+    	$operatingMonth = $this->operatingMonthRepository->getOperatingMonth($selectedMonth);
+        if($operatingMonth ==  null)
+            $operatingMonth = new OperatingMonth($selectedMonth->getYear(), $selectedMonth->getMonth());
+
         return $this->responseBuilder->asView(
             'AmbuShiftBundle:Default:shift.html.twig',
-            array("view" => new ShiftViewModel($month))
+            array("view" => new ShiftViewModel($operatingMonth))
         );
     }
 }
--- Source/tests/AmbuShiftBundle/Controller/ShiftControllerTest.php
+++ Source/tests/AmbuShiftBundle/Controller/ShiftControllerTest.php
@@ -38,4 +38,34 @@ class ShiftControllerTest extends PHPUnit_Framework_TestCase
         $result = $this->controller->indexAction();
         $this->assertEquals($response, $result);
     }
+
+    public function testIndexActionForNonExistingMonthShouldReturnNewMonth()
+    {
+        $response = "FOO";
+
+        $this->operatingMonthRepository->expects($this->once())->method('getOperatingMonth')->with($this->equalTo(new Month(2016, 1)))->will($this->returnValue(null));
+
+        $expectedData = array("view" => new ShiftViewModel(new OperatingMonth(2016, 1)));
+
+        $this->responseBuilder->expects($this->once())->method('asView')->with($this->equalTo('AmbuShiftBundle:Default:shift.html.twig'), $this->equalTo($expectedData))->will($this->returnValue($response));
+
+        $result = $this->controller->indexAction(2016, 1);
+        $this->assertEquals($response, $result);
+    }
+
+    public function testIndexActionForInvalidMonthShouldReturnCurrentMonth()
+    {
+        $response = "FOO";
+
+        $month = new OperatingMonth("2016", "05");
+        $this->calendar->expects($this->once())->method("getCurrentDate")->will($this->returnValue(new DateTime("2016-05-01")));
+        $this->operatingMonthRepository->expects($this->once())->method('getOperatingMonth')->with($this->equalTo(new Month(2016, 5)))->will($this->returnValue($month));
+
+        $expectedData = array("view" => new ShiftViewModel($month));
+
+        $this->responseBuilder->expects($this->once())->method('asView')->with($this->equalTo('AmbuShiftBundle:Default:shift.html.twig'), $this->equalTo($expectedData))->will($this->returnValue($response));
+
+        $result = $this->controller->indexAction("foo", "bar");
+        $this->assertEquals($response, $result);
+    }
 }
